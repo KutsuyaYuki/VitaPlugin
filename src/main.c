e767b278278032b7b71b63c5e059576c2f334ec9
Yuki Kutsuya <github@yuki.pet>
2023-08-23 15:29:34 +0200

    Added a title bar and content area

--- src/main.c
+++ src/main.c
@@ -5,33 +5,43 @@
 
 #include "font.h"
 
-
 vita2d_pgf *font;
 
-int mainloop()
+#define SCREEN_WIDTH 960
+#define SCREEN_HEIGHT 544
+
+#define TITLE_BAR_HEIGHT 30
+#define CONTENT_HEIGHT (SCREEN_HEIGHT - TITLE_BAR_HEIGHT)
+
+void mainloop()
 {
-  vita2d_start_drawing();
-  vita2d_clear_screen();
-
-  font_draw_string(100, 100, RGBA8(0xFF, 0xFF, 0xFF, 0xFF), "Yuki is een meem");
-
-  vita2d_end_drawing();
-  vita2d_swap_buffers();
-
-  return 1;
+    vita2d_start_drawing();
+    vita2d_clear_screen();
+    
+    // Draw title bar
+    vita2d_draw_rectangle(0, 0, SCREEN_WIDTH, TITLE_BAR_HEIGHT, RGBA8(0x00, 0x00, 0xFF, 0xFF));
+    font_draw_string(10, 5, RGBA8(0xFF, 0xFF, 0xFF, 0xFF), "Yuki's First Vita Application");
+    
+    // Draw content area
+    vita2d_draw_rectangle(0, TITLE_BAR_HEIGHT, SCREEN_WIDTH, CONTENT_HEIGHT, RGBA8(0x00, 0x00, 0x00, 0xFF));
+    font_draw_string(10, TITLE_BAR_HEIGHT + 10, RGBA8(0xFF, 0xFF, 0xFF, 0xFF), "Content Area");
+    
+    vita2d_end_drawing();
+    vita2d_swap_buffers();
 }
 
 int main()
 {
-  vita2d_init();
+    vita2d_init();
+    font = vita2d_load_default_pgf();
 
-  while (1) {
-    mainloop();
-  }
+    while (1) {
+        mainloop();
+    }
 
-  vita2d_fini();
+    vita2d_free_pgf(font);
+    vita2d_fini();
+    sceKernelExitProcess(0);
 
-  sceKernelExitProcess(0);
-
-  return 0;
+    return 0;
 }
